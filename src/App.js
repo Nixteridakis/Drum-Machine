41c3e0abf21ea0cbd4bb7e92aaf1fcef00509835
Marinos Chrysanthou <marinoschrysanthou@Marinoss-MacBook-Pro.local>
2019-01-05 17:16:59 +0100

    add linting

--- src/App.js
+++ src/App.js
@@ -1,70 +1,70 @@
-import React from 'react';
-import './App.css';
-import {Keyboard} from './Components/keyboard';
-import {patternA , patternB} from './drum-patterns';
+import React from "react"
+import "./App.css"
+import {Keyboard} from "./Components/keyboard"
+import {patternA , patternB} from "./drum-patterns"
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      power: "off",
-      display: "",
-      pattern: patternA,
-      volume: 0.4
-    };
-    this.powerSwitch = this.powerSwitch.bind(this);
-    this.patternChange = this.patternChange.bind(this);
-    this.display = this.display.bind(this);
-    this.changeVolume = this.changeVolume.bind(this);
-  }
+	constructor(props) {
+		super(props)
+		this.state = {
+			power: "off",
+			display: "",
+			pattern: patternA,
+			volume: 0.4
+		}
+		this.powerSwitch = this.powerSwitch.bind(this)
+		this.patternChange = this.patternChange.bind(this)
+		this.display = this.display.bind(this)
+		this.changeVolume = this.changeVolume.bind(this)
+	}
 
-  powerSwitch() {
-    if (this.state.power === "on") {
-      this.setState({
-        power: "off",
-        display: ""
-      });
-    } else {
-      this.setState({
-        power: "on"
-      });
-    }
-  }
+	powerSwitch() {
+		if (this.state.power === "on") {
+			this.setState({
+				power: "off",
+				display: ""
+			})
+		} else {
+			this.setState({
+				power: "on"
+			})
+		}
+	}
 
-  patternChange() {
-    this.setState({
-      pattern: this.state.pattern === patternA ? patternB : patternA
-    });
-  }
+	patternChange() {
+		this.setState({
+			pattern: this.state.pattern === patternA ? patternB : patternA
+		})
+	}
 
-  display(name) {
-    if (this.state.power === 'on'){
-    this.setState({
-      display: name
-    });
-    }
-  }
+	display(name) {
+		if (this.state.power === "on"){
+			this.setState({
+				display: name
+			})
+		}
+	}
 
-  changeVolume(e) {
-    this.display(e);
-    let num = e / 100;
-    this.setState({
-      volume: num
-    });
-  }
+	changeVolume(e) {
+		this.display(e)
+		let num = e / 100
+		this.setState({
+			volume: num
+		})
+	}
 
-  render() {
-    return (
-      <Keyboard
-        powerSwitch={this.powerSwitch}
-        pattern={this.state.pattern}
-        patternChange={this.patternChange}
-        state={this.state}
-        display={this.display}
-        changeVolume={this.changeVolume}
-      />
-    );
-  }
+	render() {
+		return (
+			<Keyboard
+				powerSwitch={this.powerSwitch}
+				pattern={this.state.pattern}
+				patternChange={this.patternChange}
+				state={this.state}
+				display={this.display}
+				changeVolume={this.changeVolume}
+			/>
+		)
+	}
 }
 
-export default App;
+export default App
--- src/Components/key.js
+++ src/Components/key.js
@@ -1,103 +1,103 @@
-import React from 'react';
+import React from "react"
 
 const activeKey = {
-  color: "#cac6c6",
-  boxShadow: "0px 1px 14px #c30000f5"
-};
+	color: "#cac6c6",
+	boxShadow: "0px 1px 14px #c30000f5"
+}
 
 const inactiveKey = {
-  color: "#f0ecec",
-  boxShadow: "0px 6px 35px #ff0000f5"
-};
+	color: "#f0ecec",
+	boxShadow: "0px 6px 35px #ff0000f5"
+}
 
 const offKey = {
-  boxShadow: "none"
-};
+	boxShadow: "none"
+}
 
 export class Key extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      keystate: inactiveKey
-    };
-    this.changeKeyColor = this.changeKeyColor.bind(this);
-    this.keyDownPress = this.keyDownPress.bind(this);
-    this.keyUpPress = this.keyUpPress.bind(this);
-    this.playSound = this.playSound.bind(this);
-  }
+	constructor(props) {
+		super(props)
+		this.state = {
+			keystate: inactiveKey
+		}
+		this.changeKeyColor = this.changeKeyColor.bind(this)
+		this.keyDownPress = this.keyDownPress.bind(this)
+		this.keyUpPress = this.keyUpPress.bind(this)
+		this.playSound = this.playSound.bind(this)
+	}
 
-  componentDidMount() {
-    document.addEventListener("keydown", this.keyDownPress);
-    document.addEventListener("keyup", this.keyUpPress);
-  }
+	componentDidMount() {
+		document.addEventListener("keydown", this.keyDownPress)
+		document.addEventListener("keyup", this.keyUpPress)
+	}
 
-  componentDidUnmount() {
-    document.addEventListener("keydown", this.keyDownPress);
-    document.addEventListener("keyup", this.keyUpPress);
-  }
+	componentDidUnmount() {
+		document.addEventListener("keydown", this.keyDownPress)
+		document.addEventListener("keyup", this.keyUpPress)
+	}
 
-  changeKeyColor() {
-    if (this.props.power === "on") {
-      if (this.state.keystate === activeKey){
-        this.setState({
-          keystate: inactiveKey
-        })
-    }
-      else{
-        this.setState({
-          keystate: activeKey
-        })
-      }
-    }
-  }
+	changeKeyColor() {
+		if (this.props.power === "on") {
+			if (this.state.keystate === activeKey){
+				this.setState({
+					keystate: inactiveKey
+				})
+			}
+			else{
+				this.setState({
+					keystate: activeKey
+				})
+			}
+		}
+	}
 
-  keyDownPress(e) {
-    if (this.props.power === "on") {
-      if (e.keyCode === this.props.keyCode) {
-        this.changeKeyColor();
-        this.playSound();
-      }
-    }
-  }
+	keyDownPress(e) {
+		if (this.props.power === "on") {
+			if (e.keyCode === this.props.keyCode) {
+				this.changeKeyColor()
+				this.playSound()
+			}
+		}
+	}
 
-  keyUpPress(e) {
-    if (this.props.power === "on") {
-      if (e.keyCode === this.props.keyCode) {
-        this.changeKeyColor();
-      }
-    }
-  }
+	keyUpPress(e) {
+		if (this.props.power === "on") {
+			if (e.keyCode === this.props.keyCode) {
+				this.changeKeyColor()
+			}
+		}
+	}
 
-  playSound(e) {
-    if (this.props.power === "on") {
-      const sound = document.getElementById(this.props.keyTrigger);
-      sound.volume = this.props.volume;
-      sound.play();
-      this.props.display(this.props.keyId);
-    }
-  }
+	playSound() {
+		if (this.props.power === "on") {
+			const sound = document.getElementById(this.props.keyTrigger)
+			sound.volume = this.props.volume
+			sound.play()
+			this.props.display(this.props.keyId)
+		}
+	}
 
-  render() {
-    return (
-      <div
-        style={(this.props.power === 'on') ? (this.state.keystate) : offKey}
-        className="drum-pad"
-        id={this.props.keyId}
-        onClick={this.playSound}
-        onMouseDown={this.changeKeyColor}
-        onMouseUp={this.changeKeyColor}
-        onKeyDown={this.keyDownPress}
-        onKeyUp={this.keyUpPress}
-      >
-        {this.props.keyTrigger}
-        <audio
-          id={this.props.keyTrigger}
-          className="clip"
-          src={this.props.audio}
-          type="audio/mp3"
-        />
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div
+				style={(this.props.power === "on") ? (this.state.keystate) : offKey}
+				className="drum-pad"
+				id={this.props.keyId}
+				onClick={this.playSound}
+				onMouseDown={this.changeKeyColor}
+				onMouseUp={this.changeKeyColor}
+				onKeyDown={this.keyDownPress}
+				onKeyUp={this.keyUpPress}
+			>
+				{this.props.keyTrigger}
+				<audio
+					id={this.props.keyTrigger}
+					className="clip"
+					src={this.props.audio}
+					type="audio/mp3"
+				/>
+			</div>
+		)
+	}
 }
 
